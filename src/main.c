dcfd5305d65b02a78eeb0e5d8556821b7a3bc672
Quang Vinh Dang <dqvinh101@gmail.com>
2025-11-20 10:29:42 -0800

    Adding ways to generate json file for LSP and convert main.c to be favour of CMSIS

--- src/main.c
+++ src/main.c
@@ -1,36 +1,20 @@
+#include "stm32f4xx.h"
 #include <stdint.h>
-
-#define PERIPHERAL_BASE (0x40000000U)
-#define AHB1_BASE (PERIPHERAL_BASE + 0x20000U)
-#define GPIOA_BASE (AHB1_BASE + 0x0U)
-#define RCC_BASE (AHB1_BASE + 0x3800U)
-
-#define RCC_AHB1ENR_OFFSET (0x30U)
-#define RCC_AHB1ENR ((volatile uint32_t *)(RCC_BASE + RCC_AHB1ENR_OFFSET))
-#define RCC_AHB1ENR_GPIOAEN (0x00U)
-
-#define GPIO_MODER_OFFSET (0x00U)
-#define GPIOA_MODER ((volatile uint32_t *)(GPIOA_BASE + GPIO_MODER_OFFSET))
-#define GPIO_MODER_MODER5 (10U)
-#define GPIO_ODR_OFFSET (0x14U)
-#define GPIOA_ODR ((volatile uint32_t *)(GPIOA_BASE + GPIO_ODR_OFFSET))
 
 #define LED_PIN 5
 
 void main(void) {
-  *RCC_AHB1ENR |= (1 << RCC_AHB1ENR_GPIOAEN);
+  // Enable clock for GPIOA peripheral
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 
-  // do two dummy reads after enabling the peripheral clock, as per the errata
-  volatile uint32_t dummy;
-
-  dummy = *(RCC_AHB1ENR);
-  dummy = *(RCC_AHB1ENR);
-
-  *GPIOA_MODER |= (1 << GPIO_MODER_MODER5);
+  // Configure the PA5 pin as output (LED on the Núcleo board)
+  GPIOA->MODER |= (1 << 10);  // MODER5[1:0] = 01 (Output)
+  GPIOA->MODER &= ~(1 << 11); // Ensure that MODER5[1] is cleared
 
   while (1) {
-    *GPIOA_ODR ^= (1 << LED_PIN);
-    for (uint32_t i = 0; i < 1000000; i++)
-      ;
+    // Toggle the LED
+    GPIOA->ODR ^= (1 << LED_PIN);
+    for (uint32_t i = 0; i < 5000000; i++)
+      ; // Simple delay
   }
 }
