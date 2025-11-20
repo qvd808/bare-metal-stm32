9cc1e6c7cbcd59a8173e36c72d6f33cbc6d7df5a
Quang Vinh Dang <dqvinh101@gmail.com>
2025-11-20 10:51:06 -0800

    Update main.c and add todo Lists for next PR

--- src/main.c
+++ src/main.c
@@ -7,7 +7,10 @@ void main(void) {
   // Enable clock for GPIOA peripheral
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 
-  // Configure the PA5 pin as output (LED on the Núcleo board)
+  // Device erreta 2.2.7 - Delay after an RCC pheripheral clock
+  volatile uint32_t tmp = RCC->AHB1ENR;
+
+  // Configure the PA5 pin as output (LED on the Nucleo board)
   GPIOA->MODER |= (1 << 10);  // MODER5[1:0] = 01 (Output)
   GPIOA->MODER &= ~(1 << 11); // Ensure that MODER5[1] is cleared
 
